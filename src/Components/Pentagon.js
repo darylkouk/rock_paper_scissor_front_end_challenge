2f0cc41b9488d3aebab80cba02846d3c1c62fd4d
darylkouk <57950357+darylkouk@users.noreply.github.com>
2022-01-21 06:51:35 -0500

    Fix pentagon icons styling

--- src/Components/Pentagon.js
+++ src/Components/Pentagon.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import pentagonImage from '../images/bg-pentagon.svg';
 import rockIcon from '../images/icon-rock.svg';
+import paperIcon from '../images/icon-paper.svg';
+import lizardIcon from '../images/icon-lizard.svg';
+import scissorIcon from '../images/icon-scissors.svg';
+import spockIcon from '../images/icon-spock.svg'
 import './Assets/Pentagon.css';
 
 function Pentagon() {
@@ -11,21 +15,21 @@ function Pentagon() {
                 <img className='pentagon-image' src={pentagonImage}></img>
                 <div className='icons-container'>
                     <div className='icon-image-one'>
-                        <img src={rockIcon}></img>
+                        <img src={scissorIcon}></img>
                     </div>
                     <div className='icon-image-two'>
-                        <img src={rockIcon}></img>
+                        <img src={spockIcon}></img>
                     </div>
                     <div className='icon-image-three'>
-                        <img src={rockIcon}></img>
+                        <img src={paperIcon}></img>
                     </div>
                     <div className='icon-image-four'>
-                        <img src={rockIcon}></img>
+                        <img src={lizardIcon}></img>
                     </div>
                     <div className='icon-image-five'>
                         <img src={rockIcon}></img>
-                </div></
-                div>
+                    </div>
+                </div>
             </div>
             
         </div>
