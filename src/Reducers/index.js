1859f1268a958939f697a8fd87462ac79b612763
darylkouk <57950357+darylkouk@users.noreply.github.com>
2022-01-22 21:22:25 -0500

    Fix combineReducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import handleModal from "./handleModal";
 import handlePlayer from "./handlePlayer";
+import handleScore from "./handleScore";
 
 export default combineReducers({
     modal: handleModal,
-    playerPick: handlePlayer
+    playerPick: handlePlayer,
+    score: handleScore
 });
