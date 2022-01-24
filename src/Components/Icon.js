e847db55a25cca80949f3f43978e05f3c0025c13
darylkouk <57950357+darylkouk@users.noreply.github.com>
2022-01-23 21:22:56 -0500

    Add winning icon highlight

--- src/Components/Icon.js
+++ src/Components/Icon.js
@@ -12,27 +12,27 @@ function Icon(props) {
         switch(type) {
             case 'scissor':
                 return(
-                    <div className='icon-scissor' style={props.hidden? {visibility:'hidden'}: {}}>
+                    <div className={props.highlight? 'icon-scissor highlight': 'icon-scissor'} style={props.hidden? {visibility:'hidden'}: {}}>
                         <img src={scissorIcon}></img>
                     </div>);
             case 'rock':
                 return(
-                    <div className='icon-rock' style={props.hidden? {visibility:'hidden'}: {}}>
+                    <div className={props.highlight? 'icon-scissor rock': 'icon-rock'} style={props.hidden? {visibility:'hidden'}: {}}>
                         <img src={rockIcon}></img>
                     </div>);
             case 'paper':
                 return(
-                    <div className='icon-paper' style={props.hidden? {visibility:'hidden'}: {}}>
+                    <div className={props.highlight? 'icon-paper highlight': 'icon-paper'} style={props.hidden? {visibility:'hidden'}: {}}>
                         <img src={paperIcon}></img>
                     </div>);
             case 'lizard':
                 return(
-                    <div className='icon-lizard' style={props.hidden? {visibility:'hidden'}: {}}>
+                    <div className={props.highlight? 'icon-lizard highlight': 'icon-lizard'} style={props.hidden? {visibility:'hidden'}: {}}>
                         <img src={lizardIcon}></img>
                     </div>);
             case 'spock':
                 return(
-                    <div className='icon-spock' style={props.hidden? {visibility:'hidden'}: {}}>
+                    <div className={props.highlight? 'icon-spock highlight': 'icon-spock'} style={props.hidden? {visibility:'hidden'}: {}}>
                         <img src={spockIcon}></img>
                     </div>);
             default:
